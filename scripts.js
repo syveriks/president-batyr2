5724add10f25db4068733bd079e8eb38e7aed21f
syveriks <iminaliev88@gmail.com>
2025-04-22 23:37:19 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const sections = document.querySelectorAll('section');
+  sections.forEach(section => {
+    section.classList.add('animated');
+  });
+});
